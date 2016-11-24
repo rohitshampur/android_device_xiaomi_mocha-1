03803fccda4ec3b6a5a2cbf4d8ebd59efce94753
tank0412 <romabukhtiarov@gmail.com>
2016-11-24 20:08:04 +0300

    mocha: initial experimental bring up

--- cm.mk
+++ cm.mk
@@ -1,8 +1,19 @@
-# Inherit device configuration for shieldtablet.
-$(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
+# Release name
+PRODUCT_RELEASE_NAME := mocha
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_shieldtablet
-PRODUCT_DEVICE := shieldtablet
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/mocha/mocha.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="xiaomi/mocha/mocha:4.4.4/KTU84P/4.12.05:user/release-keys" \
+    PRIVATE_BUILD_DESC="mocha-user 4.4.4 4.12.05 release-keys"
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mocha
+PRODUCT_NAME := cm_mocha
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := mocha
+PRODUCT_MANUFACTURER := xiaomi
