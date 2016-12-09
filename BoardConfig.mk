decd9b70e469166860b8a346daa2b87d12d5895e
Rohit Shampur <rohitshampur@users.noreply.github.com>
2016-12-09 18:27:05 +0530

    removed libinit_tn8 call

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,6 @@ BOARD_SEPOLICY_DIRS += device/xiaomi/mocha/sepolicy
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_tn8
-TARGET_LIBINIT_DEFINES_FILE := device/xiaomi/mocha/init/init_tn8.cpp
+#TARGET_INIT_VENDOR_LIB := libinit_tn8
+#TARGET_LIBINIT_DEFINES_FILE := device/xiaomi/mocha/init/init_tn8.cpp
 
